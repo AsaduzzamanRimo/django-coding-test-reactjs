def0a6f07492b0559b8b5cfee727bca65e21e06d
AsaduzzamanRimo <73344916+AsaduzzamanRimo@users.noreply.github.com>
2023-08-28 09:27:16 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react'
+import Home from './home/Home'
+
+export class App extends Component {
+  render() {
+    <Fragment>
+        <Home />
+    </Fragment>
+  }
+}
+
+export default App
